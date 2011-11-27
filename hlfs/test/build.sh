ef6178c8c2d22194324b11b9e12bd42ac82b89c3
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2011-11-27 11:07:10 +0000

    modify test for easy use

--- hlfs/test/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../output/bin/mkfs.hlfs -u local:///tmp/testenv/ -s 1048576 -b 8192 -m 1073741824
--- hlfs/test/test.c
+++ hlfs/test/test.c
@@ -3,42 +3,72 @@
 #include <string.h>
 #include "hlfs_log.h"
 
-#define LEN		(63ull * 1024ull * 1024ull)
+static gchar *uri = NULL;
+static gint request_size = 0;
+static gint total_size = 0;
+static gboolean verbose = FALSE;
+static GOptionEntry entries[] = {
+	    {"filesystem location",'u', 0, G_OPTION_ARG_STRING,   &uri, "filesystem storage uri", "FSLOC"},
+    	{"filesystem request size", 'r', 0, G_OPTION_ARG_INT, &request_size, "test request size", "REQUESTSIZE"},
+    	{"filesystem tatal size", 'm', 0, G_OPTION_ARG_INT, &total_size, "test total request size", "TOTALSIZE"},
+    	{"verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL },
+    	{NULL}
+};
 
-int main(){
-	if (log4c_init()) {
-		g_message("log4c_init error!");
-	}
-   g_print(" test init hlfs >>> \n");
-   const char * uri = "local:///tmp/testenv/testfs";
-//   const char * fs_name = "testfs";
-   HLFS_CTRL * ctrl = init_hlfs(uri);
-   g_assert(ctrl != NULL);
-   g_print(" test  hlfs open >>> \n");
-   uint64_t ret = 0;
-   ret = hlfs_open(ctrl,1);
-   g_assert(ret == 0);
-   g_print(" test  hlfs write >>> \n");
-   static char content[LEN];
-   memset(content, 0, LEN);
-#if 0
-   int offset = 0;
-   while(offset < 8192*10*1000){
-      ret = hlfs_write(ctrl,content,8192*10,offset);
-      g_assert(ret==8192*10);
-      offset +=8192*10;
-      printf("offset:%d\n",offset);
+static void 
+error_func(GOptionContext *context, GOptionGroup *group, 
+				gpointer data, GError **error) 
+{
+    if (*error && (*error)->message) {
+        gchar *progname = g_get_prgname();
+        g_print("%s: %s\nTry '%s --help' for more information.\n", 
+				progname, (*error)->message, progname);
+        exit(EXIT_FAILURE);
+    }
 }
-#endif
-	uint64_t offset = 0;
-	int i = 0;
-	for (i = 0; i < 100; i++) {
-      		ret = hlfs_write(ctrl,content,LEN,offset);
-      		g_assert(ret==LEN);
-      		offset += LEN;
-      		printf("offset:%llu\n",offset);
-	}
-   ret = hlfs_close(ctrl);
-   deinit_hlfs(ctrl);
-   return 0;
+
+
+
+int main(int argc, char *argv[]){
+    if (log4c_init()) {
+        g_message("log4c_init error!");
+    }
+    GError *error = NULL;
+    GOptionContext *context;
+    context = g_option_context_new("- hlfs test -");
+    g_option_context_add_main_entries(context, entries, NULL);
+    g_option_context_set_help_enabled(context, TRUE);
+    g_option_group_set_error_hook(g_option_context_get_main_group(context),
+            (GOptionErrorFunc)error_func);
+    if (!g_option_context_parse(context, &argc, &argv, &error)) {
+        g_message("option parsing failed: %s", error->message);
+        exit(EXIT_FAILURE);
+    }
+
+    g_print("TEST : uri%s request size:%d,total size:%d\n",uri,request_size,total_size);
+    char *content = (char*)g_malloc0(request_size);
+    HLFS_CTRL * ctrl = init_hlfs(uri);
+    g_assert(ctrl != NULL);
+    uint64_t ret = 0;
+    ret = hlfs_open(ctrl,1);
+    g_assert(ret == 0);
+    g_print("TEST  hlfs open over \n");
+    int offset = 0;
+    while(offset < total_size){
+        ret = hlfs_write(ctrl,content,request_size,offset);
+        g_assert(ret==request_size);
+        offset +=request_size;
+        printf("offset:%d\n",offset);
+    }
+    g_print("TEST  hlfs write over \n");
+    offset = 0;
+    while(offset < total_size){
+        ret = hlfs_read(ctrl,content,request_size,offset);
+        g_assert(ret==request_size);
+        offset +=request_size;
+        printf("offset:%d\n",offset);
+    }
+    ret = hlfs_close(ctrl);
+    deinit_hlfs(ctrl);
+    return 0;
 }

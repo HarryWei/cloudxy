e311a7b0896b3dc84fa7f23a168a46d300bd9db8
kelvin.xupt@gmail.com <kelvin.xupt@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2011-12-19 14:18:43 +0000

    add ss2text test

---
+++ branches/kelvin/hlfs/src/snapshot/unit_test/ss2text_test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "snapshot.h"
+
+main()
+{
+	struct snapshot *ss;
+	ss = (struct snapshot *)g_malloc0(sizeof(struct snapshot));
+	char *buf = (char *)g_malloc0(sizeof(struct snapshot) + 5);
+	ss->version = 234;
+	sprintf(ss->ss_name, "snapshot9527");
+	sprintf(ss->up_ss_name, "snapshotxx");
+	ss->ime.inode_no = 77;
+	ss->ime.inode_addr = 23424234;
+	ss2text(ss, buf);
+	g_message("buf: %s", buf);
+	g_free(ss);
+	g_free(buf);
+	return 0;
+}
+

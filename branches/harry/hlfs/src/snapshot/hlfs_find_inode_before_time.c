bc75d011783bb9970050d96ea7705c1d16dc8970
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2011-12-22 14:34:04 +0000

    all is ok for me later on

--- branches/harry/hlfs/src/snapshot/hlfs_find_inode_before_time.c
+++ branches/harry/hlfs/src/snapshot/hlfs_find_inode_before_time.c
@@ -14,8 +14,7 @@
 static int get_inode_addr_in_seg(struct back_storage *storage, 
 									uint64_t timestamp,
 									const char *segfile,
-									uint64_t *inode_addr)
-{
+									uint64_t *inode_addr) {
 	bs_file_t file = storage->bs_file_open(storage, segfile, BS_READONLY);
 	if (NULL == file) {
 		HLOG_ERROR("file open error");
@@ -48,8 +47,65 @@ out:
 	return 0;
 }
 
-int find_inode_before_time(const char *uri, uint64_t timestamp, uint64_t *inode_addr)
-{
+static GList *get_last_inode_info_in_segs(struct back_storage *storage, 
+										bs_file_info_t *infos,
+										struct seg_info *seg_infos,
+										int num_entries) {
+	int ret = 0;
+	int i = 0;
+	bs_file_t file = NULL;
+	bs_file_info_t *info = infos;
+	struct seg_info *_seg_info = seg_infos;
+	gchar **v = NULL;
+	char *endptr = NULL;
+	char *tmp_buf = NULL;
+	struct inode inode;
+	GList *list = NULL;
+
+#if 0
+	tmp_buf = g_malloc0(sizeof(char) * sizeof(struct inode));
+	if (NULL == tmp_buf) {
+		g_message("%s -- allocate error!", __func__);
+		return -1;
+	}
+#endif
+	memset(&inode, 0, sizeof(struct inode));
+	for (i = 0; i < num_entries; i++) {
+		if (g_str_has_suffix(info->name, "seg")) {
+			v = g_strsplit(info->name, ".", 2);
+			_seg_info->segno = strtoul(v[0], &endptr, 0);
+			file = storage->bs_file_open(storage, info->name, BS_READONLY);
+			if (NULL == file) {
+				g_message("%s -- open file error!", __func__);
+				ret = -1;
+				goto out;
+			}
+			uint32_t size = storage->bs_file_pread(storage, file, &inode, sizeof(struct inode),
+							info->size - sizeof(struct inode) - sizeof(struct inode_map_entry));
+			if (sizeof(struct inode) != size) {
+				g_message("%s -- read inode error!", __func__);
+				ret = -1;
+				goto out;
+			}
+			_seg_info->lmtime = inode.mtime;
+			list = g_list_append(list, _seg_info);
+			_seg_info += 1;
+		}
+		info += 1;
+	}
+out:
+	if (NULL != file) {
+		storage->bs_file_close(storage, file);
+	}
+	g_strfreev(v);
+	return list;
+}
+
+static sort_segs_with_time(struct seg_info *seg_infos, int count)  {
+	struct 
+}
+
+int find_inode_before_time(const char *uri, uint64_t timestamp, uint64_t *inode_addr) {
 	struct back_storage *storage = init_storage_handler(uri);
 	if (NULL == storage) {
 		HLOG_ERROR("init storage handler error!");
@@ -77,13 +133,15 @@ int find_inode_before_time(const char *uri, uint64_t timestamp, uint64_t *inode_
 #endif
 			count += 1;
 		}
+		info += 1;
 	}
 	struct seg_info *seg_infos = (struct seg_info *)g_malloc0(sizeof(struct seg_info) * count);
 	if (NULL == seg_infos) {
 		g_message("allocate seg_info error!");
 		return -1;
 	}
-	get_last_inode_info_in_segs(storage, infos, seg_infos, num_entries, count);
+	GList *list = NULL;
+	list = get_last_inode_info_in_segs(storage, infos, seg_infos, num_entries);
 	sort_segs_with_time(seg_infos, count);
 	int segno = 0;
 	find_seg_with_timestamp(seg_infos, timestamp, &segno);

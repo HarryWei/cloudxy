6aaeef766520898aef7781960467f32c34dfec40
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2011-12-30 03:20:26 +0000

    fix bugs

--- branches/snapshot/src/snapshot/hlfs_find_inode_before_time.c
+++ branches/snapshot/src/snapshot/hlfs_find_inode_before_time.c
@@ -20,16 +20,23 @@ get_iaddr_bytime_in_seg(struct back_storage *storage,
 						const char *segfile,
 						uint64_t *inode_addr) {
 	g_message("enter func %s", __func__);
-    uint32_t segment_size,block_size,max_fs_size;
+    uint32_t segment_size = 0;
+	uint32_t block_size = 0;
+	uint32_t max_fs_size = 0;
 	int ret = read_fs_meta(storage,&segment_size, &block_size,&max_fs_size);
-	uint64_t SEGMENT_SIZE = segment_size;
+	if (0 != ret) {
+		g_message("read fs meta data error!");
+		ret = -1;
+		goto out;
+	}
+	uint32_t SEGMENT_SIZE = segment_size;
 	bs_file_t file = storage->bs_file_open(storage, segfile, BS_READONLY);
 	if (NULL == file) {
 		g_message("file open error");
 		ret = -1;
 		goto out;
 	}
-	g_message("SEGMENT SIZE is %llu", SEGMENT_SIZE);
+	g_message("SEGMENT SIZE is %d", SEGMENT_SIZE);
 	char *tmp_buf = (char *)g_malloc0(SEGMENT_SIZE);
 	if (NULL == tmp_buf) {
 		g_message("%s -- allocate error", __func__);
@@ -48,21 +55,23 @@ get_iaddr_bytime_in_seg(struct back_storage *storage,
 	struct log_header *lh = NULL;
 	struct inode_map_entry *imap = NULL;
 	struct inode *inode = NULL;
+#if 0
 	if (0 == timestamp) {
 		lh = (struct log_header *) (tmp_buf + offset);
 		imap = (struct inode_map_entry *) (tmp_buf + lh->log_size - sizeof(struct inode_map_entry));
 		*inode_addr = imap->inode_addr;
 		goto out;
 	}
+#endif
 	while (offset < count) {
 		lh = (struct log_header *) (tmp_buf + offset);
-		//g_message("%s -- log size is %d", __func__, lh->log_size);
-		imap = (struct inode_map_entry *) (tmp_buf + lh->log_size - sizeof(struct inode_map_entry));
-		//g_message("%s -- This inode addr is %llu", __func__, imap->inode_addr);
-		inode = (struct inode *) (tmp_buf + lh->log_size - sizeof(struct inode_map_entry) - sizeof(struct inode));
+		g_message("%s -- log size is %d", __func__, lh->log_size);
+		imap = (struct inode_map_entry *) (tmp_buf + offset + lh->log_size - sizeof(struct inode_map_entry));
+		g_message("%s -- This inode addr is %llu", __func__, imap->inode_addr);
+		inode = (struct inode *) (tmp_buf + offset + lh->log_size - sizeof(struct inode_map_entry) - sizeof(struct inode));
 		g_message("%s -- tmp_time is %llu, This inode's mtime is %llu", __func__, tmp_time, inode->mtime);
 	    *inode_addr = imap->inode_addr;
-		if ((timestamp > inode->mtime)) {
+		if ((timestamp <= inode->mtime)) {
 			goto out;
 		}
 		offset += lh->log_size;
@@ -71,25 +80,34 @@ out:
 	if (NULL != file) {
 		storage->bs_file_close(storage, file);
 	}
+	g_free(tmp_buf);
+	g_message("leave func %s", __func__);
 	return ret;
 }
 
 static int 
 sort_finfo_with_time(gconstpointer linfo, gconstpointer rinfo) {
+	g_message("enter func %s", __func__);
 	uint64_t llmtime = ((bs_file_info_t *) linfo)->lmtime;
 	uint64_t rlmtime = ((bs_file_info_t *) rinfo)->lmtime;
 	if (llmtime > rlmtime) {
+		g_message("leave func %s", __func__);
 		return 1;
 	} else if (llmtime == rlmtime) {
+		g_message("leave func %s", __func__);
 		return 0;
 	} else {
+		g_message("leave func %s", __func__);
 		return -1;
 	}
 }
 
 int 
-hlfs_find_inode_before_time(const char *uri, uint64_t timestamp,uint64_t *inode_addr) {
-    if(uri==NULL){
+hlfs_find_inode_before_time(const char *uri, 
+							uint64_t timestamp,
+							uint64_t *inode_addr) {
+	g_message("enter func %s", __func__);
+    if (uri == NULL) {
         return -1;
     }
     int ret = 0;
@@ -112,27 +130,35 @@ hlfs_find_inode_before_time(const char *uri, uint64_t timestamp,uint64_t *inode_
         if (g_str_has_suffix(info->name, "seg")) {
             info_list = g_list_append(info_list,info);
 		}
-		info ++;
+		info += 1;
 	}
-
+	if (0 == g_list_length(info_list)) {
+		g_message("There is no seg file in back storage ???");
+		ret = -1;
+		goto out;
+	}
 	info_list = g_list_sort(info_list,sort_finfo_with_time);
-    for(i = 0;i<g_list_length(info_list);i++){
+	g_message("list length is %d", g_list_length(info_list));
+    for(i = 0; i < g_list_length(info_list); i++){
         info = g_list_nth_data(info_list,i);
         if(info->lmtime >= timestamp){
            break; 
         }
     }
-    if(i == 0){//timestamp < any seg's time
+#if 0
+    if(i == 0) {//timestamp < any seg's time
        ret = -1;
        goto out;
     }
-
+#endif
     /* to find inode in give seg */
 	char segfile[512];
+	memset(0, segfile, 512);
     uint32_t segno = get_segfile_no(info->name);
     build_segfile_name(segno,segfile);
     ret = get_iaddr_bytime_in_seg(storage, timestamp, segfile, inode_addr);
 out:
+	g_message("leave func %s", __func__);
 	g_list_free(info_list);
 	return ret;
 }
---
+++ branches/snapshot/src/snapshot/unittest/test_hlfs_rm_snapshot.c
@@ -0,0 +1,134 @@
+/*
+ *  hlfs/src/snapshot/unittest/test_hlfs_rm_snapshot.c
+ *  
+ *  Harry Wei <harryxiyou@gmail.com> (C) 2011
+ */
+
+#include <glib.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h> 
+#include "api/hlfs.h"
+#include "hlfs_log.h"
+
+#define REQ_SIZE 4096
+#define TOTAL_SIZE 40960
+
+typedef struct {
+	struct hlfs_ctrl *ctrl;
+} Fixture;
+
+static void 
+do_snapshot(Fixture *fixture, int i) {
+	g_message("enter func %s", __func__);
+	char buffer[128];
+	memset(buffer, 0, 128);
+	if (0 == i) {
+		sprintf(buffer, "%s%d", "snapshot", i);
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	} else if (1 == i) {
+		sprintf(buffer, "%s", " ");
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	} else if (2 == i) {
+		sprintf(buffer, "%s", "+");
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	} else if (3 == i) {
+		sprintf(buffer, "%s", "##@");
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	} else if (4 == i) {
+		sprintf(buffer, "%s", "..");
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	} else if (5 == i) {
+		sprintf(buffer, "%s", " **");
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	} else if (6 == i) {
+		sprintf(buffer, "%s", "1234");
+		g_message("%d buffer is [%s]", i, buffer);
+		int ret = hlfs_take_snapshot(fixture->ctrl, buffer);
+		g_assert(ret == 0);
+	}
+	g_message("leave func %s", __func__);
+	return ;
+}
+
+static void 
+take_snapshot(Fixture *fixture, const void *data) {
+	g_message("enter func %s", __func__);
+	char content[REQ_SIZE];
+	int offset = 0;
+	int i = 0;
+
+	memset(content, 0, REQ_SIZE);
+	while (offset < TOTAL_SIZE) {
+		int ret1 = hlfs_write(fixture->ctrl, content, REQ_SIZE, offset);
+		g_assert_cmpint(ret1, ==, REQ_SIZE);
+		do_snapshot(fixture, i);
+		offset += REQ_SIZE;
+		i += 1;
+	}
+	g_message("leave func %s", __func__);
+	return;
+}
+
+static void 
+hlfs_rm_snapshot_setup(Fixture *fixture, const void *data) {
+	g_message("enter func %s", __func__);
+	const char *uri = (const char *)data;
+	fixture->ctrl = init_hlfs(uri);
+	int ret = hlfs_open(fixture->ctrl, 1);
+	g_assert_cmpint(ret, == , 0);
+	g_assert(fixture->ctrl != NULL);
+	take_snapshot(fixture, data);
+	g_message("leave func %s", __func__);
+	return ;
+}
+
+static void
+test_hlfs_rm_snapshot(Fixture *fixture, const void *data) {
+	g_message("enter func %s", __func__);
+	const char *uri = (const char *) data;
+	int ret = 0;
+	ret = hlfs_rm_snapshot(uri, "snapshot0");
+	g_assert(ret == 0);
+	ret = hlfs_rm_snapshot(uri, "bug here");
+	g_assert(ret == 0);
+	g_message("leave func %s", __func__);
+	return ;
+}
+
+static void 
+hlfs_rm_snapshot_tear_down(Fixture *fixture, const void *data) {
+	g_message("enter func %s", __func__);
+	hlfs_close(fixture->ctrl);
+	deinit_hlfs(fixture->ctrl);
+	g_message("leave func %s", __func__);
+	return;
+}
+
+int main(int argc, char **argv) {
+	g_message("enter func %s", __func__);
+	if (log4c_init()) {
+		g_message("log4c init error!");
+	}
+	g_test_init(&argc, &argv, NULL);
+	g_test_add("/misc/hlfs_rm_snapshot", 
+				Fixture, 
+				"local:///tmp/testenv/testfs",
+				hlfs_rm_snapshot_setup, 
+				test_hlfs_rm_snapshot, 
+				hlfs_rm_snapshot_tear_down);
+	g_message("leave func %s", __func__);
+	return g_test_run();
+}

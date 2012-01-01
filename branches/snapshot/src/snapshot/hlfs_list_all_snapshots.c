0a065b51f61d0886a663dd9ee4ba7a17c3ddba17
kelvin.xupt@gmail.com <kelvin.xupt@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-01-01 06:46:02 +0000

    tidied

--- branches/snapshot/src/snapshot/hlfs_list_all_snapshots.c
+++ branches/snapshot/src/snapshot/hlfs_list_all_snapshots.c
@@ -28,13 +28,6 @@ void list_key(gpointer data, gpointer usr_data)
 		return ;
 	}
 	char tmp[128];
-#if 0
-	tmp = (char *)g_malloc0(128);
-	if (NULL == tmp) {
-		HLOG_ERROR("Allocate error!");
-		return ;
-	}
-#endif
 	memset(tmp, 0, 128);
 	sprintf(tmp, "%s\n", (char *)data);
 	size = g_strlcat(usr_data, tmp, MAX_BUFSIZE);
@@ -53,45 +46,42 @@ int hlfs_list_all_snapshots(const char *uri, char **ss_name_array)
 {
 	HLOG_DEBUG("enter func %s", __func__);
 	int ret = 0;
-#if 0
-	char *tmp_buf = NULL;
-	tmp_buf = (char *)g_malloc0(MAX_BUFSIZE);
-	if (NULL == tmp_buf) { //TODO if size of snapshot.txt > MAX_BUFSIZE
-		HLOG_ERROR("Allocate error!");
+	
+	if (NULL == *ss_name_array) { 
+		HLOG_ERROR("No buf available!");
 		ret = -1;
 		return ret;
 	}
-	*ss_name_array = tmp_buf;
-#endif
-	*ss_name_array = (char *)g_malloc0(MAX_BUFSIZE);
-	if (NULL == *ss_name_array) { 
-		HLOG_ERROR("Allocate error!");
-		ret = -1;
-		goto out;
+	if (MAX_BUFSIZE > sizeof(*ss_name_array)) {
+		HLOG_ERROR("buf size is not enough");
+		return -1;
 	}
+	memset(*ss_name_array, 0, MAX_BUFSIZE);
+
 	GHashTable *ss_hashtable = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, NULL);
 	struct back_storage *storage = init_storage_handler(uri);
 	if (NULL == storage) {
 		HLOG_ERROR("init storage handler error!");
-		ret = -1;
-		goto out;
+		ret = -2;
+		g_hash_table_destroy(ss_hashtable);
+		return ret;
 	}
 	ret = load_all_ss(storage, ss_hashtable);
 	if (ret < 0) {
 		HLOG_ERROR("load all ss error: %d", ret);
-		ret = -1;
+		ret = -3;
 		goto out;
 	}
 	if (0 == g_hash_table_size(ss_hashtable)) {
 		HLOG_DEBUG("We may have not taken snapshot yet!!!");
-		g_free(*ss_name_array);
-		*ss_name_array = NULL;
-		ret = 1;
+		sprintf(buf, "");
+		ret = 0;
 		goto out;
 	}
+	
 	GList *list = g_hash_table_get_keys(ss_hashtable);
 	if (list == NULL) {
-		HLOG_DEBUG("list NULL");
+		HLOG_ERROR("Get hash keys error");
 		ret = -4;
 		goto out;
 	}
@@ -99,7 +89,7 @@ int hlfs_list_all_snapshots(const char *uri, char **ss_name_array)
 	g_list_foreach(list, list_key, *ss_name_array);
 	HLOG_DEBUG("buf:%s", *ss_name_array);
 	if (strlen(*ss_name_array) == 0) {
-		HLOG_ERROR("ss_name_array is empty");
+		HLOG_ERROR("list keys error");
 		ret = -5;
 		goto out;
 	}
@@ -109,12 +99,9 @@ int hlfs_list_all_snapshots(const char *uri, char **ss_name_array)
 		ret = -6;
 		goto out;
 	}
-	if (*ss_name_array == NULL) {
-		HLOG_DEBUG("Buf is NULL after listing");
-		ret = -7;
-		goto out;
-	}
+
 out:
+	g_free(storage);
 	g_hash_table_destroy(ss_hashtable);
 	HLOG_DEBUG("leave func %s", __func__);
 	return ret;

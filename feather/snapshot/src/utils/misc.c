151f737a83f2d14c2100ba3888a9cee7b61aa934
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-02-03 06:24:07 +0000

    clear up dir structure

---
+++ feather/snapshot/src/utils/misc.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "address.h"
+#include "storage.h"
+#include "bs_local.h"
+#include "hlfs_log.h"
+#include "comm_define.h"
+
+uint64_t get_current_time(void)
+{
+	HLOG_DEBUG("enter func %s", __func__);
+	struct timeval t;
+
+	if (gettimeofday(&t, NULL)) {
+		HLOG_ERROR("Get current time error!");
+		return -1;
+	}
+	HLOG_DEBUG("leave func %s", __func__);
+	return (uint64_t) t.tv_sec * 1000 + t.tv_usec / 1000;	
+}
+#if 0
+char *build_segfile_name(uint64_t segno)
+{
+    	char s[64];
+    	sprintf(s, "%llu", segno);
+    	gchar * segfile_name = g_strconcat(s, ".", "seg", NULL);
+    	return segfile_name;
+}
+
+char *build_segfile_name_by_address(uint64_t storage_address)
+{
+	uint32_t segno  = get_segno(storage_address);
+    	char s[64];
+    	sprintf(s, "%d", segno);
+    	gchar * segfile_name = g_strconcat(s,".","seg",NULL);
+    	return segfile_name;
+}
+#endif 
+int  build_segfile_name(uint32_t segno, const char *segfile_name)
+{
+	HLOG_DEBUG("enter func %s", __func__);
+        if(segfile_name==NULL){
+		HLOG_ERROR("segfile_name is null");
+           return -1;
+        }
+       snprintf((char *) segfile_name,SEGMENT_FILE_NAME_MAX,"%u%s%s",segno,".","seg");
+	HLOG_DEBUG("leave func %s", __func__);
+       return 0;
+}
+
+int  build_segfile_name_by_address(uint64_t storage_address, const char*segfile_name)
+{
+	HLOG_DEBUG("enter func %s", __func__);
+        if(segfile_name==NULL){
+           return -1;
+        }
+	    uint32_t segno  = get_segno(storage_address);
+        snprintf((char *) segfile_name,SEGMENT_FILE_NAME_MAX,"%u%s%s",segno,".","seg");
+	HLOG_DEBUG("leave func %s", __func__);
+        return 0;
+}
+
+uint32_t get_segfile_no(const char * segfile)
+{
+	HLOG_DEBUG("enter func %s", __func__);
+        const gchar *basename = g_basename(segfile);
+        gchar **v = g_strsplit(basename,".",2);
+        uint32_t segno = atol(v[0]);
+        g_strfreev(v);
+	HLOG_DEBUG("leave func %s", __func__);
+        return segno;
+}
+
+
+char *read_block(struct back_storage *storage ,uint64_t storage_address,uint32_t block_size)
+{
+	HLOG_DEBUG("enter func %s", __func__);
+	HLOG_DEBUG("%s -- storage_addr is %llu", __func__, storage_address);
+    int ret = 0;
+    uint32_t offset = get_offset(storage_address); 
+    const char segfile_name[SEGMENT_FILE_NAME_MAX];
+    ret = build_segfile_name(get_segno(storage_address), (char *) segfile_name);
+    if (-1 == ret) {
+	    HLOG_ERROR("build_segfile_name error");
+	    return NULL;
+    }
+
+    bs_file_t file = storage->bs_file_open(storage,segfile_name,BS_READONLY); 
+    if(file==NULL){
+        HLOG_ERROR("can not open segment file %s",segfile_name);
+        return NULL;
+    }
+
+    char * block = (char*)g_malloc0(block_size);
+    if (NULL == block) {
+	    HLOG_ERROR("Allocate Error!");
+	    block = NULL;
+	    goto out;
+    }
+	HLOG_DEBUG("%s -- offset is %u", __func__, offset);
+	HLOG_DEBUG("%s -- block_size is %u", __func__, block_size);
+    if(block_size != storage->bs_file_pread(storage,file,block,block_size,offset)){
+	    HLOG_ERROR("bs_file_pread's size is not equal to block_size");
+        g_free(block);
+        block = NULL;
+        goto out;
+    }
+
+out:
+    storage->bs_file_close(storage,file);
+	HLOG_DEBUG("leave func %s", __func__);
+    return block;
+}
+
+/* 
+ * uri format --
+ * local:///tmp/testenv/testfs
+ * hdfs:///tmp/testenv/testfs
+ * hdfs://localhost:8020/tmp/testenv/testfs
+ * hdfs://localhost/tmp/testenv/testfs
+ * hdfs://192.168.0.1:8020/tmp/testenv/testfs
+ * */
+int parse_from_uri(const char *uri, char ** head, char** hostname ,char** dir,char** fs_name,int* port)
+{
+	HLOG_DEBUG("enter func %s", __func__);
+    gchar **v=NULL;
+    gchar **v1=NULL;
+    gchar **v2=NULL;
+    char *pre_uri = g_dirname(uri);
+    *fs_name = (char *) g_basename (uri);
+    v = g_strsplit(pre_uri,"://",2);
+    if(g_strv_length(v)!=2){
+       g_strfreev(v);
+       return -1; 
+    }
+    *head = g_strdup(v[0]);
+        
+    if( v[1][0] == '/' ){
+        HLOG_DEBUG("default localhost for hostname\n");
+        *hostname = g_strdup("default");
+        *dir = g_strdup(v[1]);
+        *port = 0; 
+    }else{
+        v1 = g_strsplit(v[1],"/",2);
+        if(g_strv_length(v1) < 2){
+           g_strfreev(v);
+           g_strfreev(v1);
+           return -1;
+        }
+        v2 = g_strsplit(v1[0],":",2);
+        if(g_strv_length(v2)!=2){
+           *hostname = g_strdup(v1[0]);
+		   char _dir[128];
+		   memset(_dir, 0, 128);
+		   sprintf(_dir, "%s%s", "/", v1[1]);
+           *dir = g_strdup(_dir);
+           *port = 8020;
+        }else{
+           *hostname = g_strdup(v2[0]);
+		   char _dir[128];
+		   memset(_dir, 0, 128);
+		   sprintf(_dir, "%s%s", "/", v1[1]);
+           *dir = g_strdup(_dir);
+           *port = atoi(v2[1]);
+        }
+    }
+    g_strfreev(v);
+    g_strfreev(v1);
+    g_strfreev(v2);
+	HLOG_DEBUG("leave func %s", __func__);
+    return 0;
+}

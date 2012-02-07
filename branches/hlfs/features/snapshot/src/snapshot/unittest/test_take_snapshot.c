fa0fd287a2b2892df8de3fc697bd14f54ccdc526
kelvin.xupt@gmail.com <kelvin.xupt@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-02-07 15:25:19 +0000

    take snapshot case3

--- branches/hlfs/features/snapshot/src/snapshot/unittest/test_take_snapshot.c
+++ branches/hlfs/features/snapshot/src/snapshot/unittest/test_take_snapshot.c
@@ -4,6 +4,7 @@
  * case1: We opened hlfs in the mode readonly.
  * case2: ssname has been used.
  * case3: normal.
+ * case4: tree snapshot test.
  * By Kelvin <kelvin.xupt@gmail.com> (c) 2012
  */
 
@@ -66,9 +67,6 @@ void case1_setup()
 		g_message("open hlfs error");
 		exit(EXIT_FAILURE);
 	}
-#if 0
-	g_message("seg fault test");
-#endif 
 }
 
 /*
@@ -158,13 +156,10 @@ void case2_setup()
 		g_message("open hlfs error");
 		exit(EXIT_FAILURE);
 	}
-#if 0
-	g_message("seg fault test");
-#endif 
 }
 
 /*
- * case1 test:
+ * case2 test:
  * --call the function hlfs_take_snapshot with the arguments ctrl & "test_snapshot";
  *   The return value should be a minus.
  */
@@ -180,13 +175,97 @@ void test_case2()
 }
 
 /*
- * case1 teardown:
+ * case2 teardown:
  * --close hlfs;
  * --deinit hlfs;
  * --free the structure case1_fixture;
  * --rm hlfs root directory;
  */
 void case2_teardown()
+{
+	hlfs_close(case2_fixture.ctrl);
+	deinit_hlfs(case2_fixture.ctrl);
+	if (case2_fixture.uri != NULL)
+		g_free(case2_fixture.uri);
+	system("cat /tmp/testenv/testfs/snapshot.txt");
+	system("rm -rf /tmp/testenv");
+}
+
+/*
+ * case3 setup:
+ * --Format hlfs;
+ * --init and open hlfs writeable;
+ */
+Fixture case3_fixture;
+void case3_setup()
+{
+	int ret = 0;
+
+	system("mkdir /tmp/testenv");
+	system("cd ../../../../build && ./build_local.sh");
+	system("cd -");
+
+	case3_fixture.uri = (char *)g_malloc0(MAX_FILE_NAME_LEN);
+	sprintf(case3_fixture.uri, URI);
+	g_message("uri: %s", case3_fixture.uri);
+	case3_fixture.ctrl = init_hlfs(case3_fixture.uri);
+	if (case3_fixture.ctrl == NULL) {
+		g_message("init_hlfs error");
+		g_assert(case3_fixture.ctrl != NULL);
+	}
+	
+	if (0 != (ret = hlfs_open(case3_fixture.ctrl, 1))) {
+		g_message("open hlfs error with flag 1");
+		exit(EXIT_FAILURE);
+	}
+}
+
+/*
+ * case3 test:
+ * --call the function hlfs_take_snapshot with the arguments ctrl & "test_snapshot(i)";
+ * we can print snapshot.txt to check wether snapshots were taken properly. 
+ */
+
+void test_case3()
+{
+	int ret = 0;
+	char *buf = (char *)g_malloc0(4096);
+	uint64_t offset = 0;
+	int i = 0;
+	char *name_buf = (char *)g_malloc0(MAX_FILE_NAME_LEN);
+
+	while (offset < 40960) {
+		if (0 > (ret = hlfs_write(case3_fixture.ctrl, buf, 4096, offset))) {
+			g_message("hlfs_write error");
+			g_free(buf);
+			g_free(name_buf);
+			exit(EXIT_FAILURE);
+		}
+		memset(name_buf, 0, sizeof(name_buf));
+		sprintf(name_buf, "test_snapshot_%d", i);
+		ret = hlfs_take_snapshot(case3_fixture.ctrl, name_buf);
+		if (ret < 0) {
+			g_message("error: snapshot exists", ret);
+			g_free(buf);
+			g_free(name_buf);
+			g_assert_cmpint(ret, ==, 0);
+		}
+		i++;
+		offset += 4096;
+	}
+	g_free(buf);
+	g_free(name_buf);
+}
+
+/*
+ * case3 teardown:
+ * --close hlfs;
+ * --deinit hlfs;
+ * --free the structure case3_fixture;
+ * --print snapshot.txt to check result.
+ * --rm hlfs root directory;
+ */
+void case3_teardown()
 {
 	hlfs_close(case2_fixture.ctrl);
 	deinit_hlfs(case2_fixture.ctrl);
@@ -213,5 +292,11 @@ int main(int argc, char **argv) {
 				case2_setup, 
 				test_case2, 
 				case2_teardown);
+	g_test_add("/misc/hlfs_take_snapshot", 
+				Fixture, 
+				NULL,
+				case3_setup, 
+				test_case3, 
+				case3_teardown);
 	return g_test_run();
 }

a22eb57f536f5102ef36ac7c3a6ea393afeddd20
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-02-23 16:29:49 +0000

    fix a bug for crfile

--- person/harry/ECMS/NodeController/scripts/vm_start_make_crfile.sh
+++ person/harry/ECMS/NodeController/scripts/vm_start_make_crfile.sh
@@ -4,12 +4,12 @@
 vm_crfile=$work_dir/$vm_id.cfg
 
 #Step 7. Make the configure file
-	echo "vcpus = $vm_cpu_count" > $vm_crfile					1>/dev/null 2>&1
+	echo "vcpus = $vm_cpu_count" > $vm_crfile				1>/dev/null 2>&1
 	echo "name = $vm_os_type.$vm_id" >> $vm_crfile				1>/dev/null 2>&1
-	echo "memory = $vm_mem_size" >> $vm_crfile					1>/dev/null 2>&1
+	echo "memory = $vm_mem_size" >> $vm_crfile				1>/dev/null 2>&1
 	echo "vif = [ 'mac=$vm_mac_addr' ]" >> $vm_crfile			1>/dev/null 2>&1
-	echo "ip = $vm_ip_addr" >> $vm_crfile						1>/dev/null	2>&1
-	echo "netmask = 255.255.255.0" >> $vm_crfile				1>/dev/null	2>&1
-	echo "vncpasswd = '$vnc_passwd'" >> $vm_crfile				1>/dev/null	2>&1
+	echo "ip = $vm_ip_addr" >> $vm_crfile					1>/dev/null 2>&1
+	echo "netmask = 255.255.255.0" >> $vm_crfile				1>/dev/null 2>&1
+	echo "vncpasswd = '$vnc_passwd'" >> $vm_crfile				1>/dev/null 2>&1
 	echo "bootloader = "/usr/bin/pygrub"" >> $vm_crfile			1>/dev/null 2>&1
-	echo "disk = [ "tap2:vhd:$sys_disk,xvda,w","tap2:aio:$os_type_dir/$vm_os_type.img,xvdb,r" ]" >> $vm_crfile		1>/dev/null 2>&1
+	echo "disk = [ "tap2:vhd:$sys_disk,xvda,rw" ]" >> $vm_crfile		1>/dev/null 2>&1

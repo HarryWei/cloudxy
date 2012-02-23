c4c410b9a3adca991180e5ffc3d98d75421dc759
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-02-23 04:16:19 +0000

    Step 2,3 tested

--- person/harry/ECMS/NodeController/scripts/vm_start_make_iso.sh
+++ person/harry/ECMS/NodeController/scripts/vm_start_make_iso.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-scene_file="/tmp/initialize"
+vm_id=$1
+vm_hostname=$2
+vm_passwd=$3
+scene_file="/tmp/init"
 scene_iso_file="$HOME/$vm_id/$vm_id.iso"
 
 
 #Step 3. Make iso image for vm_passwd and vm_hostname
-echo "hostname: $vm_hostname" > $scene_file		1>/dev/null 2>&1
-echo "password: $vm_passwd" >> $scene_file		1>/dev/null 2>&1
-mkisofs -o $scene_iso_file $scene_file			1>/dev/null	2>&1
+echo "hostname: $vm_hostname" > $scene_file;		
+echo "password: $vm_passwd" >> $scene_file;		
+mkisofs -o $scene_iso_file $scene_file		1>/dev/null 2>&1;
 if [ $? != 0 ]; then
 	echo "mkisofs error";
-	log "mkisofs error";
+	./log.sh "mkisofs error";
 	exit 1;
 fi

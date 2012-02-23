b8f754ce880c260ea55e7577d0f97f946bd1894a
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-02-23 15:30:58 +0000

    fix a bug for mkisofs

--- person/harry/ECMS/NodeController/scripts/vm_start_make_iso.sh
+++ person/harry/ECMS/NodeController/scripts/vm_start_make_iso.sh
@@ -3,14 +3,14 @@
 vm_id=$1
 vm_hostname=$2
 vm_passwd=$3
-scene_file="/tmp/init"
+scene_file="/tmp/initialize"
 scene_iso_file="$HOME/$vm_id/$vm_id.iso"
 
 
 #Step 3. Make iso image for vm_passwd and vm_hostname
 echo "hostname: $vm_hostname" > $scene_file;		
 echo "password: $vm_passwd" >> $scene_file;		
-mkisofs -o $scene_iso_file $scene_file		1>/dev/null 2>&1;
+mkisofs -r -o $scene_iso_file $scene_file		1>/dev/null 2>&1;
 if [ $? != 0 ]; then
 	echo "mkisofs error";
 	./log.sh "mkisofs error";

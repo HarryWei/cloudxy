552d7f58ff40b2e0e0de0ea0866f07e612fb870b
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-02-24 16:26:18 +0000

    Step 4 is tested ok

--- person/harry/ECMS/NodeController/scripts/vm_start_cr_sysdisk.sh
+++ person/harry/ECMS/NodeController/scripts/vm_start_cr_sysdisk.sh
@@ -9,6 +9,7 @@ sysdisk_dir="$HOME/sysdisk"
 #Step 4. Create system disk. TODO support *hdfs* pattern
 if [ -d $sysdisk_dir ]; then
 		echo "$sysdisk_dir exist"
+		source ./log.sh "log dir has existed!"
 else
 	mkdir $sysdisk_dir				1>/dev/null 2>&1
 fi
@@ -18,20 +19,21 @@ if [ -f $mkfs_hlfs ]; then
 		then
 			echo "remove $sysdisk_dir/$vm_id successfully";
 			source ./log.sh "remove $sysdisk_dir/$vm_id successfully";
-			exit 1;
 		else
 			echo "fail to remove $sysdisk_dir/$vm_id, check permission";
 			source ./log.sh "fail to remove $sysdisk_dir/$vm_id, check permission";
 			exit 1;
 		fi	
+	else
+		mkdir $sysdisk_dir/$vm_id;
 	fi
-	$mkfs_hlfs -u local:///$sysdisk_dir/$vm_id -b 8192 -s 67108864 -m 1024	1>/dev/null 2>&1
-	if [ $? != 0 ]; then
-		echo "Format block device error";
-		source ./log.sh "Format block device error";
+	$mkfs_hlfs -u local://$sysdisk_dir/$vm_id -b 8192 -s 67108864 -m 1024
+	if [ $? == -1 ]; then
+		echo "Format hlfs error";
+		source ./log.sh "Format hlfs error";
 		exit 1;
 	fi
-	tap-ctl create -a hlfs:local:///$sysdisk_dir/$vm_id		1>/dev/null 2>&1
+	tap-ctl create -a hlfs:local://$sysdisk_dir/$vm_id		1>/dev/null 2>&1
 	tap_ctl_ret=$?
 	if [ $tap_ctl_ret == -1 ]; then
 		echo "Tap-ctl execute error";

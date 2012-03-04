51dcd5f1df84ceab712585c66f7abbca2d4c7b55
kelvin.xupt@gmail.com <kelvin.xupt@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-03-04 11:51:29 +0000

    add branch cache

---
+++ features/cache/src/include/storage_helper.h
@@ -0,0 +1,28 @@
+#ifndef _HLFS_STORAGE_HELPER_H_
+#define _HLFS_STORAGE_HELPER_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include "hlfs_ctrl.h"
+#include "storage.h"
+
+#ifdef __cplusplus  
+extern "C" {
+#endif
+int deinit_storage_handler(struct back_storage * storage);
+struct inode * load_latest_inode(struct back_storage *storage);
+//struct back_storage* init_storage_handler(const char* uri,const char *fs_name);
+struct back_storage* init_storage_handler(const char* uri);
+struct inode *load_inode(struct back_storage * storage,uint64_t inode_storage_addr);
+int read_fs_meta(struct back_storage *storage,uint32_t *segment_size,uint32_t *block_size,uint32_t *max_fs_size);
+uint64_t get_last_inode_storage_addr_in_seg( struct back_storage * storage, uint64_t segno);
+int get_cur_latest_segment_info(struct back_storage * storage,uint32_t *segno,uint32_t *offset);
+uint64_t get_db_storage_addr_in_inode(struct back_storage * storage,struct inode *inode, uint64_t db_no,uint32_t block_size);
+int load_latest_inode_map_entry(struct back_storage *storage,uint32_t segno,uint32_t last_offset,struct inode_map_entry *ime);
+int file_get_contents(struct back_storage *storage,const char* filename,const char**constents,uint32_t *size);
+int file_append_contents(struct back_storage *storage,const char* filename,const char* contents,uint32_t size);
+#ifdef __cplusplus 
+} 
+#endif 
+
+#endif

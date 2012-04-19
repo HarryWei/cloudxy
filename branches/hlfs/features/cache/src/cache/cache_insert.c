faf35e6f80a6e2a909de81b90947b9c7c7d82acb
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-04-19 11:27:06 +0000

    xxxxxxxxxxx

--- branches/hlfs/features/cache/src/cache/cache_insert.c
+++ branches/hlfs/features/cache/src/cache/cache_insert.c
@@ -1,10 +1,33 @@
+#include <string.h>
 #include "cache.h"
+<<<<<<< .mine
+#include "cache_helper.h"
+#include "hlfs_log.h"
+=======
 #include "string.h"
+>>>>>>> .r1101
 
-int cache_insert(CACHE_CTRL *cache_ctrl, uint64_t block_no, char *block)
+int cache_insert(CACHE_CTRL *cache_ctrl, uint32_t start_block_no, uint32_t block_count,char *block_buf)
 {
 	HLOG_DEBUG("--Entering func %s", __func__);
 	int ret = 0;
+<<<<<<< .mine
+    if(block_count + cache_ctrl->cache_size - get_cache_free_size(cache_ctrl) 
+            < cache_ctrl->flush_trigger_level * cache_ctrl->cache_size){
+       HLOG_DEBUG("--need trigger flush worker to free cache--");
+       g_mutex_lock(cache_ctrl->cache_mutex);
+       g_cond_signal(cache_ctrl->flush_waken_cond);
+       g_mutex_unlock(cache_ctrl->cache_mutex);
+
+       g_mutex_lock(cache_ctrl->cache_mutex);
+       while(get_cache_free_size(cache_ctrl) < block_count)
+           g_cond_wait(cache_ctrl->writer_waken_cond,cache_ctrl->cache_mutex);
+       g_mutex_unlock(cache_ctrl->cache_mutex);
+    }else{
+       HLOG_DEBUG("--write to cache--");
+       write_cache(cache_ctrl,start_block_no,block_count,block_buf); 
+    }
+=======
 	block_t *_block = NULL;
 	
 	if (block == NULL) {
@@ -29,5 +52,6 @@ int cache_insert(CACHE_CTRL *cache_ctrl, uint64_t block_no, char *block)
 			(gpointer)_block->block);
 	HLOG_DEBUG("block_no: %llu inserted", _block->block_no);
 	HLOG_DEBUG("--Leaving func %s", __func__);
+>>>>>>> .r1101
 	return ret;
 }

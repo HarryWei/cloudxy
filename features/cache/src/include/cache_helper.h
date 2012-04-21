e12d001a8cce22c7185f5a6230ea7563e1df9590
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-04-21 07:37:23 +0000

    fix some bug and add ut

--- features/cache/src/include/cache_helper.h
+++ features/cache/src/include/cache_helper.h
@@ -5,7 +5,7 @@
 #include "cache.h"
 
 static int write_cache(CACHE_CTRL *cctrl,uint64_t start_block_no,uint32_t block_count,char*block_buf){
-	g_message("--enter fun %s", __func__);
+	HLOG_DEBUG("--enter fun %s", __func__);
     g_mutex_lock(cctrl->cache_mutex);
     int i;
     int idx=0;
@@ -21,7 +21,7 @@ static int write_cache(CACHE_CTRL *cctrl,uint64_t start_block_no,uint32_t block_
 }
 
 static uint32_t __get_cache_free_size(CACHE_CTRL *cctrl){
-	 g_message("--enter fun %s", __func__);
+	 HLOG_DEBUG("--enter fun %s", __func__);
      uint32_t free_block_size = cctrl->cache_size - g_queue_get_length(cctrl->dirty_block);
      return free_block_size;
 }
@@ -33,7 +33,7 @@ static uint32_t get_cache_free_size(CACHE_CTRL *cctrl){
 }
 
 static void __free_from_cache(CACHE_CTRL *cctrl,GSList *free_list){
-	g_message("--enter fun %s", __func__);
+	HLOG_DEBUG("--enter fun %s", __func__);
     int free_block_count =  g_slist_length(free_list);
     int i;
     for(i=0;i<free_block_count;i++){
@@ -50,7 +50,7 @@ static void free_from_cache(CACHE_CTRL *cctrl,GSList *free_list){
     g_mutex_unlock(cctrl->cache_mutex);
 }
 
-static int get_continues_blocks(CACHE_CTRL *cctrl,GSList *continue_block_list){
+static int get_continues_blocks(CACHE_CTRL *cctrl,GSList **continue_block_list){
     HLOG_DEBUG("--enter func %s--",__func__);
     g_mutex_lock(cctrl->cache_mutex);
     int size = g_queue_get_length(cctrl->dirty_block);
@@ -59,31 +59,40 @@ static int get_continues_blocks(CACHE_CTRL *cctrl,GSList *continue_block_list){
        g_mutex_unlock(cctrl->cache_mutex);
        return 0;
     }
+    if(*continue_block_list!=NULL){
+       HLOG_DEBUG("-- not find any dirty block --");
+       return -1;
+    }
     block_t *block = (gpointer)g_queue_peek_head(cctrl->dirty_block);
     int max_block_no = block->block_no;
     int min_block_no = block->block_no;
-    HLOG_DEBUG("--oldest block no:%lu--",block->block_no);
+    HLOG_DEBUG("--total dirty block:%d,oldest block no:%lu--",size,block->block_no);
+    *continue_block_list = g_slist_append(*continue_block_list,block);
     uint64_t count=0;
+    int idx = 1; 
     while(TRUE){
-        int tail = size - count; 
-        block_t *block = (gpointer)g_queue_peek_nth(cctrl->dirty_block,tail);
-        if(max_block_no+1!=block->block_no || min_block_no-1!=block->block_no){
-           HLOG_DEBUG("--not find continue block no:%lu--",block->block_no);
+        block_t *block = (gpointer)g_queue_peek_nth(cctrl->dirty_block,idx);
+        g_assert(block!=NULL);
+        if(max_block_no+1!=block->block_no && min_block_no-1!=block->block_no){
+           HLOG_DEBUG("--not find continue block no:%lu!--",block->block_no);
            break;
         }
         if(block->block_no == max_block_no + 1){
-           HLOG_DEBUG("--find continue block no:%lu--",block->block_no);
+           HLOG_DEBUG("--find after continue block no:%lu--",block->block_no);
            max_block_no++;
-           continue_block_list = g_slist_append(continue_block_list,block);
+           *continue_block_list = g_slist_append(*continue_block_list,block);
         }
         if(block->block_no == min_block_no - 1){
-           HLOG_DEBUG("--find continue block no:%lu--",block->block_no);
+           HLOG_DEBUG("--find before continue block no:%lu--",block->block_no);
            min_block_no--;
-           continue_block_list = g_slist_prepend(continue_block_list,block);
+           *continue_block_list = g_slist_prepend(*continue_block_list,block);
         }
-        count++;
-        if(count >= cctrl->flush_once_size){
+        if(++count >= cctrl->flush_once_size-1){
            HLOG_DEBUG("--get enought flush once count block :%lu--",cctrl->flush_once_size);
+           break;
+        }
+        if(++idx >=size-1){
+           HLOG_DEBUG("--iterator over :%d--",idx);
            break;
         }
     }

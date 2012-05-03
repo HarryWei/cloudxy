2426231a46236b057a71dcfd5319010d13098b5a
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-03 14:27:51 +0000

    wwwwwwwwwww

--- branches/hlfs/features/clean/src/include/seg_clean.h
+++ branches/hlfs/features/clean/src/include/seg_clean.h
@@ -2,7 +2,8 @@
 #define _HLFS_SEG_CLEAN_H_
 #define ALIVE_LOG_BITMAP 512
 #define SEGMENTS_USAGE_FILE "segments_usage.txt"
-#define SEGMENTS_DEL_FILE   "segments_delmark.txt"
+//#define SEGMENTS_DEL_FILE   "segments_delmark.txt"
+#include "storage.h"
 
 /* segment usage structure for cleaning task */
 typedef struct segment_usage {
@@ -22,7 +23,7 @@ extern "C" {
 #endif
 
 
-int segs_usage_status(storage_t *storage,uint64_t start_segno,uint64_t end_segno);
+int segs_usage_status(struct back_storage *storage,uint64_t start_segno,uint64_t end_segno);
 int seg_usage_calc(struct back_storage* storage, const char *segfile,struct inode * refer_inode ,SEG_USAGE_T *seg_usage,uint32_t block_size);
 int load_seg_usage(struct back_storage * storage,uint32_t segno,const char* segment_usage_file,SEG_USAGE_T * seg_usage);
 int load_all_seg_usage(struct back_storage *storage,const char *seg_usage_file,GHashTable* seg_usage_hashtable);

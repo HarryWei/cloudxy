23511163b5b20bfdd82371c3ce8790e855cc86bb
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-04 14:45:36 +0000

    xxxxxxxxxxxx

--- branches/hlfs/features/clean/src/clean/unittest/test_seg_usage_dump.c
+++ branches/hlfs/features/clean/src/clean/unittest/test_seg_usage_dump.c
@@ -1,7 +1,7 @@
 #include <glib.h>
 #include <stdlib.h>
-#include "clean_helper.h"
-#include "clean.h"
+#include "seg_clean_helper.h"
+#include "seg_clean.h"
 
 #include "comm_define.h"
 
@@ -25,8 +25,8 @@ void case_setup()
 	//system("cd ../../../../ && ./output/bin/mkfs.hlfs -u local:///tmp/testenv/testfs -b 8192 -s 67108864 -m 1024 ");
 	//system("cd -");
 	char * uri = "local:///tmp/testenv/testfs";
-	fixture->storage = init_storage_handler(uri);
-    g_assert(NULL != fixture->storage);
+	fixture.storage = init_storage_handler(uri);
+    g_assert(NULL != fixture.storage);
 }
 
 /*
@@ -36,23 +36,37 @@ void case_setup()
  */
 
 /*  write trigger wb*/
-void test_seg_usage2text()
+void test_seg_usage_text()
 {
 	int ret = 0;
 	SEG_USAGE_T seg_usage;
-	seg_usage->segno = 10;
-	strcpy(seg_usage->up_sname,"test_snapshot",strlen("test_snapshot"));
-	seg_usage->block_num = 64;
-	seg_usage->inode_addr = 10101010;
-	seg_usage->log_num = 8;
-	seg_usage->timestamp = get_current_time();
-	seg_usage->alive_block_num = 10;
-	seg_usage->bitmap = g_malloc0(sizeof(int)*100);
+	seg_usage.segno = 10;
+	strncpy(seg_usage.up_sname,"test_snapshot",strlen("test_snapshot"));
+	seg_usage.block_num = 64;
+	seg_usage.inode_addr = 10101010;
+	seg_usage.log_num = 8;
+	seg_usage.timestamp = get_current_time();
+	seg_usage.alive_block_num = 10;
+	seg_usage.bitmap = g_malloc0((seg_usage.log_num-1)/sizeof(gint)+1);
+    int i,idx;
+    i = 0;idx = i/sizeof(gint);
+    seg_usage.bitmap[idx] |= 1<<0%sizeof(gint);
+    i = 2;idx = i/sizeof(gint);
+    seg_usage.bitmap[idx] |= 1<<2%sizeof(gint);
+    i = 7;idx = i/sizeof(gint);
+    i = 0;idx = i/sizeof(gint);
+    seg_usage.bitmap[idx] |= 1<<7%sizeof(gint);
 	char textbuf[4096];
 	memset(textbuf,4096,0);
     ret = seg_usage2text(&seg_usage,textbuf);
 	g_assert(ret > 0);
-	g_free(seg_usage->bitmap);
+    printf("textbuf is :%s\n",textbuf);
+	g_free(seg_usage.bitmap);
+    memset(&seg_usage,sizeof(SEG_USAGE_T),0);
+    ret = seg_usage4text(&seg_usage,textbuf);
+    g_assert(ret == 0);
+    g_assert(seg_usage.segno == 10);
+    g_assert(seg_usage.inode_addr == 10101010);
 }
 
 
@@ -65,7 +79,7 @@ void test_seg_usage2text()
  */
 void case_teardown()
 {
-	deinit_storage_handler(fixture->storage);
+	deinit_storage_handler(fixture.storage);
 }
 
 int main(int argc, char **argv) {
@@ -74,11 +88,11 @@ int main(int argc, char **argv) {
 	}
 	g_test_init(&argc, &argv, NULL);
 #if 1
-	g_test_add("/misc/test_seg_usage2text", 
+	g_test_add("/misc/test_seg_usage_text", 
 				Fixture, 
 				NULL,
 				case_setup, 
-				test_seg_usage2text, 
+				test_seg_usage_text, 
 				case_teardown);
 #endif
 #if 0

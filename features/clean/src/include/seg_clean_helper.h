341b3de7107a542e15811e9200fc14f1f8efd711
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-04 14:15:59 +0000

    xxxxxxxxx

--- features/clean/src/include/seg_clean_helper.h
+++ features/clean/src/include/seg_clean_helper.h
@@ -16,10 +16,12 @@ extern "C" {
 #endif
 
 //int segment_delmark2text(uint32_t segno,char *textbuf);
-int seg_usage2text(struct segment_usage* segment_usage,char *textbuf);
-int dump_seg_usage_text(struct back_storage * storage,const char*segment_usage_file, const char * seg_usage_text);
-int load_seg_usage_text(struct back_storage *storage,SEG_USAGE_T *seg_usage, const char *textbuf);
-int load_all_seg_usage  (struct back_storage *storage,    const char *seg_usage_file,  GHashTable* seg_usage_hashtable);
+int seg_usage2text(SEG_USAGE_T* seg_usage,char *textbuf);
+int seg_usage4text(SEG_USAGE_T * seg_usage, const char *textbuf);
+
+//int dump_seg_usage_text(struct back_storage * storage,const char*segment_usage_file, const char * seg_usage_text);
+//int load_seg_usage_text(struct back_storage *storage,SEG_USAGE_T *seg_usage, const char *textbuf);
+int load_all_seg_usage  (struct back_storage *storage, const char *seg_usage_file,  GHashTable* seg_usage_hashtable);
 int sort_all_seg_usage(GHashTable *ss_hashtable,GList *seg_usage_list);
 int get_refer_inode_between_snapshots(struct back_storage *storage,uint64_t segno,GList *snapshot_sorted_list, struct inode ** inode);
 

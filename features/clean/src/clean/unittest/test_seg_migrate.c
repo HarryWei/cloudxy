189f6d939714cf7b8955e5bc055bd9efd6a2d42d
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-05 13:54:57 +0000

    add some ut

--- features/clean/src/clean/unittest/test_seg_migrate.c
+++ features/clean/src/clean/unittest/test_seg_migrate.c
@@ -20,7 +20,7 @@ Fixture fixture;
 
 void case_setup()
 {
-#if 1
+#if 0
 	system("rm -rf /tmp/testenv");
 	system("mkdir /tmp/testenv -p");
 	system("cd ../../../ && ./output/bin/mkfs.hlfs -u local:///tmp/testenv/testfs -b 8192 -s 67108864 -m 1024");
@@ -68,15 +68,24 @@ void case_setup()
  */
 
 /*  write trigger wb*/
-void test_seg_usage_calc()
+void test_seg_migrate()
 {
+    int ret=0;
     char * uri = "local:///tmp/testenv/testfs";
-    struct back_storage *storage = init_storage_handler(uri);
-    uint32_t segment_size = 0;
-    uint32_t block_size = 0;
-    uint32_t max_fs_size = 0;
-    int ret = read_fs_meta(storage,&segment_size, &block_size,&max_fs_size);
+    fixture.hctrl = init_hlfs(uri);
+    g_assert(fixture.hctrl != NULL);
+    ret = hlfs_open(fixture.hctrl,1);
     g_assert(ret == 0);
+    //return ;
+    struct back_storage *storage = fixture.hctrl->storage;
+    uint32_t block_size = fixture.hctrl->sb.block_size;
+    printf("hctrl block size:%d\n",block_size);
+    //struct back_storage *storage = init_storage_handler(uri);
+    //uint32_t segment_size = 0;
+    //uint32_t block_size = 0;
+    //uint32_t max_fs_size = 0;
+    //int ret = read_fs_meta(storage,&segment_size, &block_size,&max_fs_size);
+    //g_assert(ret == 0);
     GHashTable   * ss_hashtable = g_hash_table_new_full(g_str_hash,g_str_equal,NULL,NULL);
     ret = load_all_snapshot(storage,"snapshot.txt",ss_hashtable);
     printf("snapshot loaded\n"); 
@@ -87,7 +96,8 @@ void test_seg_usage_calc()
     g_assert(ss_list !=NULL);
     g_assert(ret == 0);
     int i;
-    for(i=13;i<17;i++){
+    int max_seg = fixture.hctrl->last_segno;
+    for(i=0;i<=max_seg;i++){
         struct inode * inode=NULL;
         ret = get_refer_inode_between_snapshots(storage,i,ss_list,&inode);
         printf("segno :%d ret:%d\n",i,ret);
@@ -108,9 +118,20 @@ void test_seg_usage_calc()
         }
         if(ret == 2){
            printf("seg is above snapshot,maybe need migrate\n");
+           SEG_USAGE_T seg_usage;
+           memset(&seg_usage,0,sizeof(SEG_USAGE_T));
+           ret = seg_usage_calc(storage,block_size,i,&fixture.hctrl->inode,&seg_usage);
+           g_assert(ret ==0);
+           char textbuf[4096];
+           memset(textbuf,4096,0);
+           ret = seg_usage2text(&seg_usage,textbuf);
+           g_assert(ret > 0);
+           printf("textbuf is :%s\n",textbuf);
+           //ret = migrate_alive_blocks(fixture.hctrl,&seg_usage);
+           //g_assert(ret == 0);
         }
     }
-
+    ret = hlfs_close(fixture.hctrl);
 }
 
 
@@ -133,11 +154,11 @@ int main(int argc, char **argv) {
 	}
 	g_test_init(&argc, &argv, NULL);
 #if 1
-	g_test_add("/misc/test_seg_usage_calc", 
+	g_test_add("/misc/test_seg_migrate", 
 				Fixture, 
 				NULL,
 				case_setup, 
-				test_seg_usage_calc, 
+				test_seg_migrate, 
 				case_teardown);
 #endif
 #if 0

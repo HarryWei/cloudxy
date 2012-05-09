89f27d4c6d50251cd462da3f4428bfe05c13ba3a
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-09 14:48:59 +0000

    xxxxxxxxxxxxxxxxx

--- branches/hlfs/features/icache/src/include/icache.h
+++ branches/hlfs/features/icache/src/include/icache.h
@@ -38,7 +38,7 @@ int icache_init(ICACHE_CTRL *icache_ctrl,
 //int icache_insert_blocks(CACHE_CTRL *cache_ctrl, uint32_t start_block_no, uint32_t block_count,char *block_buf);
 int icache_insert_iblock(ICACHE_CTRL *icache_ctrl, uint32_t iblock_no, char *iblock_buf);
 int icache_query_iblock(ICACHE_CTRL *icache_ctrl, uint64_t iblock_no, char *iblock_buf);
-gboolean  icache_iblock_exist(ICACHE_CTRL *icache_ctrl, uint64_t iblock_no);
+//gboolean  icache_iblock_exist(ICACHE_CTRL *icache_ctrl, uint64_t iblock_no);
 int lcache_destroy(ICACHE_CTRL *icache_ctrl);
 
 

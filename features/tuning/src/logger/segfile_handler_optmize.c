f7a2d96e0bbb8e9e98b01b8c361b56094d6bfd39
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-15 06:23:45 +0000

    xxxxxx

--- features/tuning/src/logger/segfile_handler_optmize.c
+++ features/tuning/src/logger/segfile_handler_optmize.c
@@ -94,7 +94,7 @@ int prev_open_wsegfile(struct hlfs_ctrl *ctrl){
 
 
 
-char *read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address)
+int read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address,char* block)
 {
 	HLOG_DEBUG("enter func %s", __func__);
     int ret = 0;
@@ -105,14 +105,14 @@ char *read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address)
 	uint32_t block_size = ctrl->sb.block_size;
     if(0!=prev_open_rsegfile(ctrl,segno)){
 	   HLOG_ERROR("can not pre open read segfile:%u",segno);
-       return NULL; 
+       return -1; 
     }
-    char * block = (char*)g_malloc0(block_size);
-    if (NULL == block) {
-	    HLOG_ERROR("Allocate Error!");
-	    block = NULL;
-	    goto out;
-    }
+    //char * block = (char*)g_malloc0(block_size);
+    //if (NULL == block) {
+	//    HLOG_ERROR("Allocate Error!");
+	//    block = NULL;
+	//    goto out;
+    //}
     int retry_time = 3;
     do{
         //if(block_size != (write_size = ctrl->storage->bs_file_pread(ctrl->storage,ctrl->last_rsegfile_handler,block,block_size,offset))){
@@ -121,12 +121,15 @@ char *read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address)
             HLOG_ERROR("can not read block from seg:%u#%u :ret :%d",segno,offset,write_size);
             g_usleep(2000);
             retry_time--;
+			ret = -1;
+        }else{
+            ret = 0;
         }
     }while(write_size < block_size && retry_time >0);
 out:
     //pre_close_read_segfile(ctrl,segno);
 	HLOG_DEBUG("leave func %s", __func__);
-    return block;
+    return ret;
 }
 
 

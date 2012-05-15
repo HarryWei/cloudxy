e6d9505b6c281f22a76b0c36ce091fdb8babd7a7
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-15 01:28:48 +0000

    fix bug of lock

--- trunk/hlfs/src/storage/init_hlfs.c
+++ trunk/hlfs/src/storage/init_hlfs.c
@@ -19,8 +19,8 @@ int flush_log(struct hlfs_ctrl *ctrl,const char *db_buff,uint32_t db_start,uint3
 	HLOG_DEBUG("enter func %s", __func__);
    
     HLOG_DEBUG("last segno: %u last offset: %u", ctrl->last_segno, ctrl->last_offset);
-    /* todo , use RCU lock instead of mutex */
     //g_mutex_lock (ctrl->hlfs_access_mutex);
+    ctrl->last_write_timestamp = get_current_time();
     int size = append_log(ctrl,db_buff,db_start,db_end);
     //g_mutex_unlock (ctrl->hlfs_access_mutex);
     if(size < 0){

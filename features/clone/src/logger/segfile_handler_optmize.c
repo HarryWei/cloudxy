dd2d91b5deade44c9940e34b8a4a9cebbae10856
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-30 15:53:33 +0000

    xxxxxxx

--- features/clone/src/logger/segfile_handler_optmize.c
+++ features/clone/src/logger/segfile_handler_optmize.c
@@ -24,67 +24,68 @@
 #include "clone.h"
 
 int prev_open_rsegfile(struct hlfs_ctrl *ctrl,uint32_t segno){
-	//HLOG_DEBUG("enter func %s", __func__);
+    //HLOG_DEBUG("enter func %s", __func__);
     struct back_storage * storage =NULL;
     if(NULL == ctrl->last_rsegfile_handler){
-       const char segfile_name[SEGMENT_FILE_NAME_MAX];
-       build_segfile_name(segno,segfile_name);
-	bs_file_t file;   
-
-       if(segno >= ctrl->start_segno){
-               storage = ctrl->storage;
-       }else{
-             if(NULL == (storage = get_parent_storage(ctrl->family,segno))){
-		  return -1;
-	      }
-       }
-	file = storage->bs_file_open(storage,segfile_name,BS_READONLY); 
-       if(file==NULL){
+        const char segfile_name[SEGMENT_FILE_NAME_MAX];
+        build_segfile_name(segno,segfile_name);
+        bs_file_t file;   
+        if(segno >= ctrl->start_segno){
+            storage = ctrl->storage;
+        }else{
+            if(NULL == (storage = get_parent_storage(ctrl->family,segno))){
+                g_assert(0);
+                return -1;
+            }
+        }
+        file = storage->bs_file_open(storage,segfile_name,BS_READONLY); 
+        if(file==NULL){
             HLOG_ERROR("can not open segment file %s",segfile_name);
             g_assert(0);
             return -1;
-       }
-          ctrl->last_rsegfile_handler = file;
-	   ctrl->last_rsegfile_offset = ctrl->last_offset;
-	   ctrl->last_read_segno =segno;
+        }
+        ctrl->last_rsegfile_handler = file;
+        ctrl->last_rsegfile_offset = ctrl->last_offset;
+        ctrl->last_read_segno =segno;
     }else if(ctrl->last_read_segno != segno || (ctrl->last_read_segno == segno && ctrl->last_rsegfile_offset != ctrl->last_offset)){
-          HLOG_DEBUG("cur segno:%d is ,last segno no:%d, last rsegfile offset:%d,last offset:%d - need close old and open new segfile",
-	   	           segno,ctrl->last_read_segno,ctrl->last_rsegfile_offset,ctrl->last_offset);
-       /* close last seg file handler...  */
-	if(ctrl->last_read_segno >= ctrl->start_segno){
-               storage = ctrl->storage;
-       }else{
-             if(NULL == (storage = get_parent_storage(ctrl->family,ctrl->last_read_segno))){
-		  return -1;
-	      }
-       }
-       if(0!=storage->bs_file_close(storage,ctrl->last_rsegfile_handler)){
-	   	    //HLOG_ERROR(" close last seg file handler error");
-			return -1;
-       }	
-       /* open cur seg file handler...  */
-       const char segfile_name[SEGMENT_FILE_NAME_MAX];
-       build_segfile_name(segno,segfile_name);
-	if(segno >= ctrl->start_segno){
-               storage = ctrl->storage;
-       }else{
-             if(NULL == (storage = get_parent_storage(ctrl->family,segno))){
-		  return -1;
-	      }
-       }
-       bs_file_t file = storage->bs_file_open(storage,segfile_name,BS_READONLY); 
-       if(file == NULL){
-            //HLOG_ERROR("can not open segment file %s",segfile_name);
-	     g_assert(0);
+        HLOG_DEBUG("cur segno:%d is ,last segno no:%d, last rsegfile offset:%d,last offset:%d - need close old and open new segfile",
+                segno,ctrl->last_read_segno,ctrl->last_rsegfile_offset,ctrl->last_offset);
+        /* close last seg file handler...  */
+        if(ctrl->last_read_segno >= ctrl->start_segno){
+            storage = ctrl->storage;
+        }else{
+            if(NULL == (storage = get_parent_storage(ctrl->family,ctrl->last_read_segno))){
+                g_assert(0);
+                return -1;
+            }
+        }
+        if(0!=storage->bs_file_close(storage,ctrl->last_rsegfile_handler)){
+            g_assert(0);
             return -1;
-       }
-       ctrl->last_rsegfile_handler = file;
-	ctrl->last_rsegfile_offset = ctrl->last_offset;
-	ctrl->last_read_segno =segno;
+        }	
+        /* open cur seg file handler...  */
+        const char segfile_name[SEGMENT_FILE_NAME_MAX];
+        build_segfile_name(segno,segfile_name);
+        if(segno >= ctrl->start_segno){
+            storage = ctrl->storage;
+        }else{
+            if(NULL == (storage = get_parent_storage(ctrl->family,segno))){
+                g_assert(0);
+                return -1;
+            }
+        }
+        bs_file_t file = storage->bs_file_open(storage,segfile_name,BS_READONLY); 
+        if(file == NULL){
+            //HLOG_ERROR("can not open segment file %s",segfile_name);
+            g_assert(0);
+            return -1;
+        }
+        ctrl->last_rsegfile_handler = file;
+        ctrl->last_rsegfile_offset  = ctrl->last_offset;
+        ctrl->last_read_segno       = segno;
     }else{
-       HLOG_DEBUG("using pre open read file handler");
+        HLOG_DEBUG("using pre open read file handler");
     }
-	
     //HLOG_DEBUG("leave func %s", __func__);
     return 0;
 }
@@ -124,7 +125,7 @@ int prev_open_wsegfile(struct hlfs_ctrl *ctrl){
 
 int read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address,char* block)
 {
-	//HLOG_DEBUG("enter func %s", __func__);
+    //HLOG_DEBUG("enter func %s", __func__);
     int ret = 0;
     struct back_storage * storage =NULL;
     int write_size = 0;
@@ -133,8 +134,8 @@ int read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address,char* block)
     HLOG_DEBUG("offset :%u,segno:%u,last_offset:%u,last_rsegfile_offset:%u",offset,segno,ctrl->last_offset,ctrl->last_rsegfile_offset);
     uint32_t block_size = ctrl->sb.block_size;
     if(0!=prev_open_rsegfile(ctrl,segno)){
-	   //HLOG_ERROR("can not pre open read segfile:%u",segno);
-       return -1; 
+        //HLOG_ERROR("can not pre open read segfile:%u",segno);
+        return -1; 
     }
     //char * block = (char*)g_malloc0(block_size);
     //if (NULL == block) {
@@ -146,28 +147,29 @@ int read_block_fast(struct hlfs_ctrl *ctrl,uint64_t storage_address,char* block)
     do{
         //if(block_size != (write_size = ctrl->storage->bs_file_pread(ctrl->storage,ctrl->last_rsegfile_handler,block,block_size,offset))){
         if(segno >= ctrl->start_segno){
-               storage = ctrl->storage;
-       }else{
-             if(NULL == (storage = get_parent_storage(ctrl->family,segno))){
-		  return -1;
-	      }
-       }
-	write_size = storage->bs_file_pread(storage,ctrl->last_rsegfile_handler,block,block_size,offset);
-       if(write_size!=block_size){
+            storage = ctrl->storage;
+        }else{
+            if(NULL == (storage = get_parent_storage(ctrl->family,segno))){
+                g_assert(0);
+                return -1;
+            }
+        }
+        write_size = storage->bs_file_pread(storage,ctrl->last_rsegfile_handler,block,block_size,offset);
+        if(write_size!=block_size){
             HLOG_WARN("can not read block from seg:%u#%u :ret :%d",segno,offset,write_size);
             g_usleep(1000);
             retry_time--;
-			ret = -1;
+            ret = -1;
         }else{
             HLOG_DEBUG("read block from seg:%u#%u size:%d",segno,offset,write_size);
             ret = 0;
         }
     }while(write_size < block_size && retry_time >0);
 out:
-    //pre_close_read_segfile(ctrl,segno);
-	//HLOG_DEBUG("leave func %s", __func__);
-    return ret;
-}
+        //pre_close_read_segfile(ctrl,segno);
+        //HLOG_DEBUG("leave func %s", __func__);
+        return ret;
+    }
 
 
 

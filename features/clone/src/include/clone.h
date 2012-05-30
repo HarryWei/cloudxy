ef0cc28a56a71864530b5f9283e10fdabfcb8d16
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-30 09:09:34 +0000

    xxxxxxxxxx

---
+++ features/clone/src/include/clone.h
@@ -0,0 +1,28 @@
+/*
+  *  Copyright (C) 2012 KangHua <kanghua151@gmail.com>
+  *
+  *  This program is free software; you can redistribute it and/or modify it
+  *  under the terms of the GNU General Public License version 2 as published by
+  *  the Free Software Foundation.
+ */
+
+#ifndef __CLONE_H__
+#define __CLONE_H__
+
+//#include "api/hlfs.h"
+#include "glib.h"
+#include "hlfs_log.h"
+#include "comm_define.h"
+
+typedef  struct family_ctrl{
+           char*      father_uri;
+	    uint64_t  base_father_inode;
+	    uint32_t  start_segno;
+           GTree *  seg_storage_map;
+ }FAMILY_CTRL;
+
+
+struct back_storage * get_parent_storage(FAMILY_CTRL *fctrl,uint32_t segno);
+int  famliy_destroy(FAMILY_CTRL *fctrl);
+
+

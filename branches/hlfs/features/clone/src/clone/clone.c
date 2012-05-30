964e457dfcf629f04e88ee1390750200e678bb84
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-30 09:09:34 +0000

    xxxxxxxxxx

---
+++ branches/hlfs/features/clone/src/clone/clone.c
@@ -0,0 +1,31 @@
+/*
+  *  Copyright (C) 2012 KangHua <kanghua151@gmail.com>
+  *
+  *  This program is free software; you can redistribute it and/or modify it
+  *  under the terms of the GNU General Public License version 2 as published by
+  *  the Free Software Foundation.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <glib.h>
+#include "hlfs_ctrl.h"
+#include "hlfs_log.h"
+#include "misc.h"
+#include "comm_define.h"
+#include "storage.h"
+#include "storage_helper.h"
+#include "bs_local.h"
+#include "seg_clean.h"
+#include "logger.h"
+
+
+struct back_storage * get_parent_storage(FAMILY_CTRL *fctrl, uint32_t segno){
+           struct back_storage * storage = NULL;
+	    return storage;
+}
+
+ 

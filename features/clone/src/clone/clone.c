cd26548168a8b9111e3f34779282ed13a62289a7
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-05-31 03:28:45 +0000

    xxx

--- features/clone/src/clone/clone.c
+++ features/clone/src/clone/clone.c
@@ -99,7 +99,7 @@ int family_destroy(FAMILY_CTRL *fctrl){
 	  return 0;
     }
 
-    i = 0;
+    int i = 0;
     for(i = g_list_length(fctrl->seg_storage_list)-1; i >= 0; i--){
         STORAGE_ITEM *storage_item = g_list_nth_data(fctrl->seg_storage_list,i);
         g_free(storage_item);

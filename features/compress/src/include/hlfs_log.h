c18e4a3e5db54e837da5d73f41b0539dea952cc7
kanghua151@gmail.com <kanghua151@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-07-07 04:01:31 +0000

    x

--- features/compress/src/include/hlfs_log.h
+++ features/compress/src/include/hlfs_log.h
@@ -14,8 +14,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "glib.h"
-#include "gthread.h"
-
 
 #define LOG_LEN				4096U
 static char __msg_log[LOG_LEN]={0};
@@ -24,7 +22,8 @@ static char *__hlog_path = NULL;
 static char __log4cfile[128] = {0};
 static int __is_init_log_path = 0;
 
-static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
+//static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
+#define G_LOCK_DEFINE_STATIC(__hlfs_log_mutex__)
 
 #define HLOG_NOTICE(msg, args...) { 															\
 	if (0 == __is_init_log_path) {																\
@@ -38,12 +37,10 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 		__is_init_log_path = 1;																	\
 	}																							\
     if (NULL != __mycat) {																		\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                         \
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);		\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat, &locinfo, LOG4C_PRIORITY_NOTICE, __msg_log, ##args);	\
-        g_static_mutex_unlock (&__hlfs_log_mutex__); 														\
 	}else {																						\
 		printf(msg, ##args);																	\
 		printf("\n");																			\
@@ -62,12 +59,10 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 				__is_init_log_path = 1;															\
 			}																					\
 	if (NULL != __mycat) {																		\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                         \
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);		\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat, &locinfo, LOG4C_PRIORITY_TRACE, __msg_log, ##args);		\
-        g_static_mutex_unlock (&__hlfs_log_mutex__);                                                       \
 	} else {																					\
 		printf(msg, ##args);																	\
 		printf("\n");																			\
@@ -86,12 +81,10 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 				__is_init_log_path = 1;															\
 			}																					\
 	if (NULL != __mycat) {																		\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                         \
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);		\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat, &locinfo, LOG4C_PRIORITY_FATAL, __msg_log, ##args);		\
-        g_static_mutex_unlock (&__hlfs_log_mutex__);                                                       \
 	} else {																					\
 		printf(msg, ##args);																	\
 		printf("\n");																			\
@@ -110,14 +103,14 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 				__is_init_log_path = 1;																	\
 			}																							\
 	if (NULL != __mycat) {																					\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                           \
-        printf("--enter lock region--");\
+		G_LOCK(__hlfs_log_mutex__)  \
+        printf("--enter lock region--")\
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);		\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat, &locinfo, LOG4C_PRIORITY_DEBUG, __msg_log, ##args);		\
 		printf("--exit lock region --\n");\
-        g_static_mutex_unlock (&__hlfs_log_mutex__);                                                           \
+		G_UNLOCK(__hlfs_log_mutex__)  \
 	} else {																					\
 		printf(msg, ##args);																	\
 		printf("\n");																			\
@@ -136,12 +129,10 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 				__is_init_log_path = 1;																	\
 			}																							\
 	if (NULL != __mycat) {																					\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                           \
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);		\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat, &locinfo, LOG4C_PRIORITY_INFO, __msg_log, ##args);		\
-        g_static_mutex_unlock (&__hlfs_log_mutex__);                                                           \
 	} else {																					\
 		printf(msg, ##args);																	\
 		printf("\n");																			\
@@ -160,12 +151,10 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 		__is_init_log_path = 1;																	\
 	}																							\
 	if (NULL != __mycat) {																		\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                         \
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);		\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat,&locinfo, LOG4C_PRIORITY_ERROR, __msg_log, ##args);		\
-        g_static_mutex_unlock (&__hlfs_log_mutex__);                                                       \
 	} else {																					\
 		printf(msg, ##args);																	\
 		printf("\n");																			\
@@ -184,12 +173,10 @@ static GStaticMutex __hlfs_log_mutex__ = G_STATIC_MUTEX_INIT;
 		__is_init_log_path = 1;															\
 	}																							\
 	if (NULL != __mycat) {																		\
-        g_static_mutex_lock (&__hlfs_log_mutex__);                                                         \
 		memset(__msg_log, 0, LOG_LEN);															\
 		snprintf(__msg_log, LOG_LEN, "[%p][%s][%s][%d]%s", g_thread_self(),__FILE__, __func__, __LINE__, msg);	\
 		const log4c_location_info_t locinfo = LOG4C_LOCATION_INFO_INITIALIZER(NULL);\
 		log4c_category_log_locinfo(__mycat, &locinfo, LOG4C_PRIORITY_WARN, __msg_log, ##args);		\
-        g_static_mutex_unlock (&__hlfs_log_mutex__);                                                       \
 	} else {																					\
 		printf(msg, ##args);																	\
 		printf("\n");																			\

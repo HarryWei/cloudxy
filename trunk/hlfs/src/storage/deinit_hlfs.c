d1a1e9df3c02d24b4d2b30fd02a99375e7e917e0
kelvin.xupt@gmail.com <kelvin.xupt@gmail.com@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2012-10-20 02:39:41 +0000

    fix double free bug

--- trunk/hlfs/src/storage/deinit_hlfs.c
+++ trunk/hlfs/src/storage/deinit_hlfs.c
@@ -22,23 +22,23 @@
 int deinit_hlfs(struct hlfs_ctrl * ctrl)
 {
 	//HLOG_DEBUG("enter func:%s",__func__);
-	if (NULL == ctrl) {
+	if (!ctrl) {
 		HLOG_ERROR("ctrl is null");
 		return -1;
     }
-    if(0!=deinit_storage_handler(ctrl->storage)){
+    if (deinit_storage_handler(ctrl->storage)){
 		HLOG_ERROR("deinit_hlfs error");
        	return -1; 
     }
-    if(ctrl->cctrl!=NULL){
+    if(ctrl->cctrl){
        //HLOG_DEBUG("-destroy cache-");
        cache_destroy(ctrl->cctrl);
     }
-    if(ctrl->icache!=NULL){
+    if(ctrl->icache){
        //HLOG_DEBUG("-destroy icache-");
        icache_destroy(ctrl->icache);
     }
-    if(ctrl->family != NULL){
+    if(ctrl->family){
 	  family_destroy(ctrl->family);
     }		
     ctrl->ctrl_region->is_start_clean = 0;
@@ -47,8 +47,8 @@ int deinit_hlfs(struct hlfs_ctrl * ctrl)
     //g_async_queue_unref(ctrl->write_rsp_aqueue);
     g_mutex_free(ctrl->hlfs_access_mutex);
     /* fix it :we do not free ctrl_region now */
-    g_free(ctrl->storage);
-    g_free(ctrl);
+    if (ctrl)
+		g_free(ctrl);
     //HLOG_DEBUG("leave func:%s",__func__);
     return 0;
 } 

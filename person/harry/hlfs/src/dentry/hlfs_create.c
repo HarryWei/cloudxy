26d4d5e486549d17b8a2e3069606d562b8ae9580
harryxiyou <harryxiyou@8f717bb2-1fde-2800-3b51-3e1dc26c946c>
2013-03-10 08:43:14 +0000

    finish touch hlfs file jobs...

---
+++ person/harry/hlfs/src/dentry/hlfs_create.c
@@ -0,0 +1,64 @@
+/*
+  *  Copyright (C) 2013 Harry Wei <harryxiyou@gmail.com>
+  *  
+  *  This program is free software; you can redistribute it and/or modify it
+  *  under the terms of the GNU General Public License version 2 as published by
+  *  the Free Software Foundation.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <glib.h>
+#include <string.h>
+#include "hlfs_ctrl.h"
+#include "dentry.h"
+#include "dentry_helper.h"
+#include "storage_helper.h"
+#include "hlfs_log.h"
+#include "misc.h"
+#include "comm_define.h"
+
+extern uint64_t INODE_NO;
+
+/*f_path: /xxx/dir/file
+  1, Check if f_path exists.
+  2, Check if /xxx/dir ecists.
+ ? 3, Update /xxx/dir log to append file.
+ ? 4, Append /xxx/dir/file log.
+  5, Append file's dentry infos to dentry file.
+ */
+int hlfs_create(struct hlfs_ctrl *ctrl, const char *f_path) {
+	g_message("9999 enter func %s", __func__);
+    if(ctrl == NULL || f_path ==NULL){
+		g_message("parameter error!");
+        return -1;
+    }
+	if ('/' != f_path[0]) {
+		g_message("please use absolute path.\n");
+		return -1;
+	}
+    int ret = 0;
+	struct dentry *ds = NULL;
+	gchar *dir_name = g_path_get_dirname(f_path);
+	if (1 == load_dentry_by_name(ctrl->storage, DENTRY_FILE, &ds, dir_name)) {
+		g_message("%s not exist, please create first.", dir_name);
+		ret = -1;
+		goto out;
+	}
+	if (1 != load_dentry_by_name(ctrl->storage, DENTRY_FILE, &ds, f_path)) {
+		g_message("%s exist, replace a new file name.", f_path);
+		ret = -1;
+		goto out;
+	}
+	struct dentry _dentry;
+	sprintf(_dentry.file_name, "%s", f_path);
+	_dentry.inode_no = ++INODE_NO;
+	ret = dump_dentry(ctrl->storage, DENTRY_FILE, &_dentry);
+	if (0 > ret) {
+		g_message("dump dentry error.");
+		goto out;
+	}
+out:
+	g_free(dir_name);
+	return ret;
+}
